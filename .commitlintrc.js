c471b1823089f64f6451998cfa3bf7729e4d13d3
ishakdeveloper <ishak.development@outlook.com>
2025-01-09 16:11:44 +0100

    feat: fix git hooks setup

--- .commitlintrc.js
+++ .commitlintrc.js
@@ -8,19 +8,18 @@ module.exports = {
         'feat', // New feature
         'fix', // Bug fix
         'docs', // Documentation
-        'style', // Formatting, missing semi colons, etc
-        'refactor', // Code change that neither fixes a bug or adds a feature
-        'test', // Adding tests
-        'chore', // Maintenance tasks, upgrades
-        'perf', // Performance improvements
-        'ci', // CI/CD related changes
-        'build', // Build system changes
-        'revert', // Revert a commit
+        'style', // Formatting
+        'refactor', // Code restructuring
+        'test', // Tests
+        'chore', // Maintenance
+        'perf', // Performance
+        'ci', // CI/CD
+        'build', // Build
+        'revert', // Revert changes
       ],
     ],
     'type-case': [2, 'always', 'lower'],
     'type-empty': [2, 'never'],
-    'scope-case': [2, 'always', 'lower'],
     'subject-empty': [2, 'never'],
     'subject-full-stop': [2, 'never', '.'],
     'header-max-length': [2, 'always', 72],
