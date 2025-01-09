7525dce930d6a83c77da526561cfd6439b870a13
ishakdeveloper <ishak.development@outlook.com>
2025-01-09 21:06:57 +0100

    test

--- ws/lib/ws_web/controllers/health_controller.ex
+++ ws/lib/ws_web/controllers/health_controller.ex
@@ -1,1 +1,7 @@
- 
+defmodule WSWeb.HealthController do
+  use WSWeb, :controller
+
+  def check(conn, _params) do
+    json(conn, %{status: "healthy"})
+  end
+end
--- ws/lib/ws_web/router.ex
+++ ws/lib/ws_web/router.ex
@@ -1,1 +1,13 @@
- 
+defmodule WSWeb.Router do
+  use WSWeb, :router
+
+  pipeline :api do
+    plug :accepts, ["json"]
+  end
+
+  scope "/", WSWeb do
+    pipe_through :api
+
+    get "/health", HealthController, :check
+  end
+end
