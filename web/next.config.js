a8ce1b39bd51d28136a951a0591ac18145828418
ishakdeveloper <ishak.development@outlook.com>
2025-01-10 10:25:12 +0100

    feat: refactor

--- web/next.config.js
+++ web/next.config.js
@@ -1,6 +1,12 @@
 /** @type {import('next').NextConfig} */
 const nextConfig = {
   output: 'standalone',
+  poweredByHeader: false,
+  assetPrefix: process.env.NEXT_PUBLIC_URL,
+  images: {
+    unoptimized: true,
+    domains: ['concord-web.fly.dev', 'staging.concord.fly.dev'],
+  },
 };
 
 module.exports = nextConfig;
